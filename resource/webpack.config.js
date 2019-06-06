b04499f1b171d31527b07ec577f089b7376e8caf
Mofon001 <mofon001@gmail.com>
2019-06-06 18:12:02 +0900

    no message

--- resource/webpack.config.js
+++ resource/webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
 	entry: './src/public/index.ts',
 	output: {
 		filename: 'bundle.js',
-		path: path.resolve(__dirname, 'dist/public')
+		path: path.resolve(__dirname, 'dist/public/js')
 	},
 	devtool: 'source-map',
 	module: {
