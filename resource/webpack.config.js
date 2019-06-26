09b1f1d189bf52105247c7948cc79443352f21db
SoraKumo <info@croud.jp>
2019-06-26 19:43:30 +0900

    サンプルテンプレートの修正

--- resource/webpack.config.js
+++ resource/webpack.config.js
@@ -1,41 +1,54 @@
 const path = require('path');
 
 module.exports = {
-	mode: 'production',
-	entry: './src/public/index.ts',
-	output: {
-		filename: 'bundle.js',
-		path: path.resolve(__dirname, 'dist/public')
-	},
-	devtool: 'source-map',
-	module: {
-		rules: [{
-				test: /\.js\.jsx$/,
-				use: ["source-map-loader"],
-				enforce: "pre"
-			},
-			{
-				test: /\.ts|\.tsx$/,
-				use: 'ts-loader'
-			}, {
-				test: /\.(scss|css)$/,
-				use: [
-					'style-loader',
-					'css-loader',
-					'sass-loader'
-				]
-			},
-		{
-			test: /\.(jpg|png|svg|gif)$/,
-			loaders: 'url-loader'
-		},
-		]
-	},
-	resolve: {
-		moduleExtensions: ['node_modules']
-	},
-	devServer: {
-		contentBase: path.join(__dirname, 'dist/public'),
-		host: "localhost"
-	},
+  mode: 'production',
+  entry: './src/public/index.ts',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist/public')
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [{
+      test: /\.(js|jsx)$/,
+      use: ["source-map-loader"],
+      enforce: "pre"
+    },
+    {
+      test: /\.(ts|tsx)$/,
+      use: 'ts-loader'
+    }, {
+      test: /\.(scss|css)$/,
+      use: [
+        'style-loader',
+        'css-loader',
+        'sass-loader'
+      ]
+    },
+    {
+      test: /\.(jpg|png|svg|gif)$/,
+      loaders: 'url-loader'
+    },
+    ]
+  },
+  resolve: {
+    extensions: [
+      ".ts",
+      ".js",
+      ".tsx",
+      ".jsx",
+      ".scss",
+      ".sass",
+      ".jpeg",
+      ".jpg",
+      ".png",
+      ".svg",
+      ".gif"
+    ],
+    moduleExtensions: ['node_modules']
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist/public'),
+    host: "localhost"
+  },
 };
